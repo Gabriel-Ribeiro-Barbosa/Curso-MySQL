045f9bf08c4a9351e3247ee707b739c69599e5b8
Gabriel Ribeiro <gribeirobarbosa386@gmail.com>
2025-03-07 20:33:52 -0300

    commit to main

---
+++ 3 aula.sql
@@ -0,0 +1,8 @@
+insert into pessoas
+(nome, nascimento, sexo, peso, altura, nacionalidade)
+values
+('Jorge-Alba','1987-03-25','M','83.5','1.79','Espanha'),
+('Maldini','1989-04-23','M','78.5','1.86','Italia'),
+('Nicolas-Nettoo','2012-04-29','M','40.0','1.60','Brasil');
+
+select *from pessoas;
